841da4853809a83aee94db5e4095048305ea75ad
Doc Walker <doc.walker@jameshardie.com>
2014-07-26 07:17:48 -0500

    Fix #98 Fix failing default_spec
    - update the way platform_family is defined in spec
    - spec currently does not check for recipe inclusion in non-rhel
      platform_family's

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -2,42 +2,29 @@
 require 'spec_helper'
 
 describe 'bootstrap::default' do
-  before do
-    # required for travis-ci
-    stub_command('sudo -V').and_return(true)
-  end # before
+  [
+    { :platform => 'centos', :version => '6.5' },
+    { :platform => 'fedora', :version => '20' }
+  ].each do |i|
+    context "#{i[:platform]}/#{i[:version]}" do
+      before do
+        # required for travis-ci
+        stub_command('sudo -V').and_return(true)
+      end # before
 
-  %w(rhel fedora).each do |platform_family|
-    describe "platform family: #{platform_family}" do
       let(:chef_run) do
-        ChefSpec::Runner.new do |node|
-          node.set['platform_family'] = platform_family
+        ChefSpec::Runner.new(i) do |node|
         end.converge(described_recipe)
       end # let
 
+      #------------------------------------------------------ include_recipe[]
       describe 'bootstrap::rhel' do
         it 'includes described recipe' do
           expect(chef_run).to include_recipe(subject)
         end # it
       end # describe
 
-    end # describe
-  end # %()
+    end # context
+  end # [...].each
 
-  %w(debian fake_platform).each do |platform_family|
-    describe "platform family: #{platform_family}" do
-      let(:chef_run) do
-        ChefSpec::Runner.new do |node|
-          node.set['platform_family'] = platform_family
-        end.converge(described_recipe)
-      end # let
-
-      describe 'bootstrap::rhel' do
-        it 'does not include recipe' do
-          expect(chef_run).to_not include_recipe(subject)
-        end # it
-      end # describe
-
-    end # describe
-  end # %()
 end # describe
